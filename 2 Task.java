11f5e9b34200ac5be16bbc55b6f717c9a8a46db3
YuliiaSpitsina <114913321+YuliiaSpitsina@users.noreply.github.com>
2022-10-31 22:23:34 +0300

    Update 2 Task.java

--- 2 Task.java
+++ 2 Task.java
@@ -1,1 +1,58 @@
+package OOP;
+
+class Hero {
+    String name;
+    public Hero (String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void attackEnemy(){
+        System.out.println(name + " атакує ворога");
+    }
+}
+
+class Warrior extends Hero {
+    Warrior(String name) {
+        super(name);
+    }
+    public void attackEnemy(){
+        System.out.println(name + " розмахує мечом");
+    }
+}
+
+class Mage extends Hero {
+    Mage(String name) {
+        super(name);
+    }
+    public void attackEnemy(){
+        System.out.println(name + " викликає стовб світла");
+    }
+}
+class Archer extends Hero {
+   public Archer(String name) {
+        super(name);
+    }
+    public void attackEnemy(){
+        System.out.println(name + " стріляє с лука");
+    }
+}
+
+public class TrainingGround {
+    public static void main(String[] args) {
+        Hero hero1 = new Hero("Foggy");
+        hero1.attackEnemy();
+
+        Warrior hero2 = new Warrior("Rory");
+        hero2.attackEnemy();
+
+        Mage hero3 = new Mage("Soul");
+        hero3.attackEnemy();
+
+        Archer hero4 = new Archer("John");
+        hero4.attackEnemy();
+    }
+}
 
